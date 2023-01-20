623d8a2cc096bae6f19e59f44593e26570912d4c
Nicole Tai <nicole1999224@gmail.com>
2023-01-20 20:33:33 +0100

    add and edit season and episode

--- app-service/anime-reminder/app/ui/src/static/js/anime.js
+++ app-service/anime-reminder/app/ui/src/static/js/anime.js
@@ -6,45 +6,46 @@ $("document").ready(function(){
         url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime",
         headers: {"Authorization": "Bearer " + keycloak.access_token },
         data: {
-            anime_id: anime_id
+            "anime_id": anime_id
         }
     }).done(function (rsp) {
         var reminder = rsp[0].anime_reminder;
         for (var i = 0; i < reminder.length; i++){
             var row = `
             <tr>
-                <td><h2>${reminder[i].season}</h2></td>
-                <td><h2>${reminder[i].episode}</h2></td>
+                <td><h2><a href="#addar" rel="modal:open" style="text-decoration: none">${reminder[i].season}</a></h2></td>
+                <td><h2><a href="#addar" rel="modal:open" style="text-decoration: none">${reminder[i].episode}</a></h2></td>
                 <td id="deltd"><h3 id="delh1">x<h3></td>
             </tr>`;
             $("#artbbody").append(row);
         }
         // location.reload();
     });
+    var sspinner = $("#sspinner").spinner();
+    var espinner = $("#espinner").spinner();
     // // var animelist = ["Spy x Famile", "Chainsaw Man"]
-    // $("#addanimebtn").click(function(){
-
-    //     // send anime name to backend
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime",
-    //         headers: {"Authorization": "Bearer " + keycloak.access_token },
-    //         data: JSON.stringify(payload),
-    //         dataType: "json",
-    //         contentType: "application/json"
-    //     }).done(function (rsp) {
-    //         // add new anime on page
-    //         var row = `
-    //         <tr id="${rsp.anime_id}tr">
-    //             <td><h2>${rsp.anime_name}</h2></td>
-    //             <td class="deltd" id="deltd"><h3 id="delh1">x<h3></td>
-    //         </tr>`;
-    //         $("#animetb1").append(row);
-    //         location.reload();
-    //     });
-    //     // clean the input box
-    //     $('input[id=addanimeinput]').val(' ');
-    // });
+    $("#addarbtn").click(function(){
+        var payload = {
+            "anime_reminder": [
+                {
+                    "season": sspinner.spinner("value").toString(),
+                    "episode": espinner.spinner("value").toString()
+                }
+            ]
+        }
+        $.ajax({
+            type: "PUT",
+            url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime/" + anime_id,
+            headers: {"Authorization": "Bearer " + keycloak.access_token },
+            data: JSON.stringify(payload),
+            dataType: "json",
+            contentType: "application/json"
+        }).done(function (rsp) {
+            location.reload();
+        });
+        // // clean the input box
+        // $('input[id=addanimeinput]').val(' ');
+    });
     // // $("tr").click(function(){
     // //     alert("Clicked");
     // //     // window.location.href = "anime.html";
