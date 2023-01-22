80b3f02364f353d7935e7f80ff8b08f8b3220744
Yung-Sheng Tu <yssamtu@gmail.com>
2023-01-22 22:14:10 +0100

    beautify the submit ui

--- app-service/anime-reminder/app/ui/src/static/js/home.js
+++ app-service/anime-reminder/app/ui/src/static/js/home.js
@@ -16,41 +16,36 @@ $("document").ready(function(){
     });
     // var animelist = ["Spy x Famile", "Chainsaw Man"]
     $("#addanimebtn").click(function(){
-        var payload = {
-            anime_name: $('input[id=addanimeinput]').val()
-        }
-        // send anime name to backend
-        $.ajax({
-            type: "POST",
-            url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime",
-            headers: {"Authorization": "Bearer " + keycloak.access_token },
-            data: JSON.stringify(payload),
-            dataType: "json",
-            contentType: "application/json"
-        }).done(function (rsp) {
-            // add new anime on page
-            var row = `
-            <tr id="${rsp.anime_id}tr">
-                <td><h2>${rsp.anime_name}</h2></td>
-                <td class="deltd" id="deltd"><h3 id="delh1">x<h3></td>
-            </tr>`;
-            $("#animetb1").append(row);
-            location.reload();
+        $(this).addClass("is_active");
+        $(this).animate({opacity: 1}, 500, function(){
+            var payload = {
+                anime_name: $('input[id=addanimeinput]').val()
+            }
+            // send anime name to backend
+            $.ajax({
+                type: "POST",
+                url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime",
+                headers: {"Authorization": "Bearer " + keycloak.access_token },
+                data: JSON.stringify(payload),
+                dataType: "json",
+                contentType: "application/json"
+            }).done(function (rsp) {
+                // add new anime on page
+                var row = `
+                <tr id="${rsp.anime_id}tr">
+                    <td><h2>${rsp.anime_name}</h2></td>
+                    <td class="deltd" id="deltd"><h3 id="delh1">x<h3></td>
+                </tr>`;
+                $("#animetb1").append(row);
+                location.reload();
+            });
+            // clean the input box
+            $('input[id=addanimeinput]').val(' ');
         });
-        // clean the input box
-        $('input[id=addanimeinput]').val(' ');
+
     });
     $("#animetb1").on('click', '.deltd', function(){
         let anime_id = $(this).siblings(".animetd").attr("id").toString();
-        // let episode = $(this).siblings(".episode").text();
-        // let payload = {
-        //     "anime_reminder": [
-        //         {
-        //             "season": season,
-        //             "episode": episode
-        //         }
-        //     ]
-        // }
         $.ajax({
             type: "DELETE",
             url: "/animereminder/api/v1/user/" + keycloak.user_id + "/anime/" + anime_id,
@@ -60,8 +55,4 @@ $("document").ready(function(){
             location.reload();
         });
     })
-    // $("tr").click(function(){
-    //     alert("Clicked");
-    //     // window.location.href = "anime.html";
-    // });
 });
